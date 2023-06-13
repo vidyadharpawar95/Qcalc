daad72b9f5df8b1eab4fa1c7d92792c831610a5c
vidyadhar <vidyadharpawar95@gmail.com>
2023-06-13 11:37:40 +0530

    Qcalc

---
+++ app/src/main/java/com/crio/qcalc/App.java
@@ -0,0 +1,20 @@
+
+package com.crio.qcalc;
+
+
+public class App {
+
+   public static void main(String[] args) {
+
+       System.out.println("Starting QCalc..");   
+       StandardCalculator calc = new StandardCalculator();
+       calc.add(Double.MAX_VALUE, Double.MAX_VALUE);
+       calc.printResult();
+       
+   }
+public Object getGreeting() {
+    return "app should have a greeting";
+}
+
+
+}
